cb71df28c21b5e3d2738416e2a5104d5b13292df
Areliz Oyarce <arelizuwc@gmail.com>
2022-04-12 17:09:57 -0500

    aplicando estilos a pagina de LogIn y register

--- src/Components/Register.js
+++ src/Components/Register.js
@@ -2,33 +2,37 @@ export const register = () => {
     const registerTemplate = `
     <section class="registerLogin">
         <div>
-            <h1>Help Potatoes</h1>
+            <h1 class="helpPotatoesTittle">Help Potatoes</h1>
+            <p class="registerTittle">Registrate</p>
+            <p class="pIconRegister">Elige el icono con el que te identifiques</p>
         </div>
-        <div>
-            <img src="">
-            <img src="">
-            <img src="">
-            <img src="">
-            <img src="">
+        <div class="papasImg">
+            <img src="./img/papamamá.png">
+            <img src="./img/papapapá.png">
+            <img src="./img/pape.png">
         </div>
         <div class="registerContainer">
-            <p>Registrate</p>
-            <div>
+            <div class= "inputContainerRegister">
                 <input type="text" id="inputName" placeholder="Usuario">
                 <input type="email" id="inputEmail" placeholder="Correo electrónico">
                 <p id="errorEmail" class="error"></p>
-                <input type="password" id="inputPassword">
+                <input type="password" id="inputPassword"placeholder="Contraseña">
                 <p id="passError" class="error"></p>
-                <button id="buttonRegister">Registrar</button>
-                <section id="modalEmailV" class="verificationEmailModal" style="display:none">
-                <p>Para verificar tu correo revisa tu bandeja de entrada
-                </p>
-                </section>
-                <p>Registrate con Google</p>
-                <img src="" alt="google logo" id="googleImg">
-                <p>¿Ya tienes cuenta?</p>
-                <button id="backLogin">Iniciar Sesión</button>
+                <button id="buttonRegister" class="buttonnextPage">Registrar</button>
             </div>
+            <div class="withGoogle">
+            <p class="pGoogle">Registrate con Google</p>
+            <img src="./img/googleLogo.png" alt="google logo" id="googleImg" class="logoGoogle">
+            <p class="pGoogle">¿Ya tienes cuenta?</p>
+            <button id="backLogin" class="backpage">Iniciar Sesión</button>
+            </div>
+            <section id="modalEmailV" class="verificationEmailModal" style="display:none">
+            <div class="containerModal">
+            <p>Para verificar tu correo revisa tu bandeja de entrada</p>
+            <button class="okaybtnModal" id="closeModal">Aceptar</button>
+            </div>
+            </section>
+            
         </div>
     </section>   
      `;
