d281c50230f1dc0648630aae34a5d65a0b328931
lilianaEstefanyPachari <lilianapachari21@gmail.com>
2022-05-04 16:50:32 -0500

    corregir la identacion y otros detalles de DOMevents, firebaseUtil y routes

--- src/lib/firebaseUtils.js
+++ src/lib/firebaseUtils.js
@@ -1,35 +1,30 @@
-//LINKS CONFIGURACIÓN
-export { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
-export  { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore.js";
+// LINKS CONFIGURACIÓN
+export { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js';
+export { getFirestore } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore.js';
 
-//LINKS AUTH
-export  {
-    getAuth,
-    createUserWithEmailAndPassword,
-    updateProfile,
-    sendEmailVerification,
-    GoogleAuthProvider,
-    signInWithPopup,
-    signInWithEmailAndPassword,
-    signOut
-}
-from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js';
-
-//LINKS FIRESTORE
+// LINKS AUTH
 export {
-    collection,
-    addDoc,
-    serverTimestamp,
-    onSnapshot,
-    query,
-    orderBy,
-    deleteDoc,
-    doc,
-    getDoc,
-    updateDoc,
-} from "https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore.js";
+  getAuth,
+  createUserWithEmailAndPassword,
+  updateProfile,
+  sendEmailVerification,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signInWithEmailAndPassword,
+  signOut,
+}
+  from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js';
 
-
-
-
-
+// LINKS FIRESTORE
+export {
+  collection,
+  addDoc,
+  serverTimestamp,
+  onSnapshot,
+  query,
+  orderBy,
+  deleteDoc,
+  doc,
+  getDoc,
+  updateDoc,
+} from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore.js';
